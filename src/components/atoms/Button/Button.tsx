cd8c7f9ab424d9ac099e3bcdc9555e964d480b31
André Canuto <andrecanutoc@gmail.com>
2026-02-20 16:26:24 -0300

    feat: adiciona estilos aos botoes

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import clsx from 'clsx';
+import styles from './Button.module.scss';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export const Button: React.FC<ButtonProps> = ({ children, className, ...props }) => {
+  return (
+    <button className={clsx(styles.button, className)} {...props}>
+      {children}
+    </button>
+  );
+};
